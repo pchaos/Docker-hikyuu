c1fdd5214d2013f9972679c168999599e1ce241b
pchaosgit <drifthua@gmail.com>
2017-07-16 21:46:41 +0800

    调整脚本。 目前基本上能完整编译hikyuu。还有最后一个错误：ImportError: /home/fasiondog/hikyuu/tools/hikyuu/_hikyuu.so: undefined symbol: _ZTIN5boost6python7objects21py_function_impl_baseE

--- sh/30_buildhikyuu.sh
+++ sh/30_buildhikyuu.sh
@@ -5,7 +5,7 @@ export BOOST_ROOT=$HOME//boost_1_64_0
 export BOOST_LIB=/usr/local/lib
 export HIKYUU=$HOME/hikyuu
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:$HOME/hikyuu/tools/hikyuu/:/usr/local/lib
-export CPLUS_INCLUDE_PATH=/opt/conda/include/python3.6m/:$HOME/hikyuu/extern-libs/sqlite3/:$HOME/hikyuu/extern-libs/mysql/include/:/usr/include/:/usr/include/hdf5/serial/
+export CPLUS_INCLUDE_PATH=/opt/conda/include/python3.6m/:$HOME/hikyuu/extern-libs/sqlite3/:$HOME/hikyuu/extern-libs/mysql/include/:/usr/include/:/usr/include/hdf5/serial/:/usr/include/log4cplus/
 ln -s /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6  /usr/lib/x86_64-linux-gnu/libsqlite3.so
 
 
