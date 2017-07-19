3b5ab95d563923fe5cae7b7254cbb9fb790eaa61
pchaosgit <drifthua@gmail.com>
2017-07-19 13:58:57 +0800

    fix bugs

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ using docker build hikyuu
 
 ## 安装完成后，运行：
 
- *   ./start.sh （或 sudo ./start.sh）
+ *   ./start.sh （或 sudo ./start.sh   不使用sudo运行docker参见： https://docs.docker.com/engine/installation/linux/linux-postinstall/）
  *   在docker中输入 jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root ，
  *   浏览器（docker之外的主系统）中输入 127.0.0.1：8888， 即可使用jupyter
 
@@ -31,11 +31,15 @@ using docker build hikyuu
 
 
 --------------------------------------------------
-remarks:
+## remarks:
 
 git add Dockerfile README.md build.sh cleanUnUsedDocker.sh sh/10_SetUpBasicEnvironment.sh sh/15_buildboost.sh sh/20_build_talib.sh sh/25_buildlog4plus.sh \
-  sh/30_buildhikyuu.sh start.sh sh/30_patchfile.hikyuu
+  sh/30_buildhikyuu.sh sh/30_patchfile.hikyuu start.sh
 
 To run Docker, your machine must have a 64-bit operating system running Windows 7 or higher. Additionally, you must make sure that virtualization is enabled on your machine.
 
 * https://docs.docker.com/toolbox/toolbox_install_windows/#step-2-install-docker-toolbox
+
+### Docker基础技术
+* http://coolshell.cn/articles/17010.html
+* http://coolshell.cn/articles/17029.html
--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ buildName="hikyuu:hikyuutest"
 log4c="log4cplus-1.2.1-rc2.tar.gz"
 [ ! -f $log4c ] && wget -c -O $log4c https://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.2.1/log4cplus-1.2.1-rc2.tar.gz/download  &
 #[ ! -f hdf5-1.8.19.tar.gz ] && wget -c -O hdf5-1.8.19.tar.gz https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-1.8.19.tar.gz &
-[ ! -f sh/conda3.sh ] && wget -c -O sh/onda3.sh https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh && chmod a+x conda3.sh
-[ ! -f ta-lib-0.4.0-src.tar.gz ] && wget -c -O ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-[ ! -f boost_1_64_0.tar.gz ] && wget -c -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download
-[ ! -d hikyuu ] && git clone https://git.oschina.net/fasiondog/hikyuu.git
+[ ! -f ta-lib-0.4.0-src.tar.gz ] && wget -c -O ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz &
+[ ! -f boost_1_64_0.tar.gz ] && wget -c -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download &
+[ ! -d hikyuu ] && git clone https://git.oschina.net/fasiondog/hikyuu.git &
+[ ! -f sh/conda3.sh ] && wget -c -O sh/conda3.sh https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh && chmod a+x conda3.sh
 [ -d archives ] && rm -rf lock partial
 [ ! -d archives ] && mkdir archives
 TINI_VERSION=v0.15.0
